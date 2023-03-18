87660621729becd506ed068dcd513b2e89d9a9ea
Quentin <q_cozic@hotmail.fr>
2023-03-18 16:50:59 +0100

    First commit

---
+++ src/main/java/com/example/test160323/Test160323Application.java
@@ -0,0 +1,13 @@
+package com.example.test160323;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test160323Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test160323Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test160323/Test160323ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test160323;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test160323ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
